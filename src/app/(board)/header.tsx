581cb4742f046cdf22b1a5e27ec46def84b7055b
Daniel <danieldcc.colares@gmail.com>
2026-06-07 11:27:48 -0300

    chore: adds search params with nuqs

--- src/app/(board)/header.tsx
+++ src/app/(board)/header.tsx
@@ -1,7 +1,24 @@
+"use client"
+
 import { LogInIcon, SearchIcon } from "lucide-react"
+import { debounce, parseAsString, useQueryState } from "nuqs"
+import type { ChangeEvent } from "react"
 import { Input } from "@/components/input"
 
 export function Header() {
+  const [search, setSearch] = useQueryState(
+    "q",
+    parseAsString.withDefault("").withOptions({
+      shallow: false,
+    }),
+  )
+
+  function handleSearchUpdate(event: ChangeEvent<HTMLInputElement>) {
+    setSearch(event.target.value, {
+      limitUrlUpdates: event.target.value !== "" ? debounce(500) : undefined,
+    })
+  }
+
   return (
     <div className="max-w-225 mx-auto w-full flex items0center justify-between">
       <div className="space-y-1">
@@ -18,13 +35,14 @@ export function Header() {
             type="text"
             placeholder="search for features..."
             className="w-67.5 pl-8"
+            value={search}
+            onChange={handleSearchUpdate}
           />
         </div>
 
         <button
           type="button"
-          className="size-8 rounded-full bg-navy-700 border border-navy-500 cursor-pointer
-           flex items-center justify-center hover:bg-navy-600 transition-colors duration-150 "
+          className="size-8 rounded-full bg-navy-700 border border-navy-500 cursor-pointer flex items-center justify-center hover:bg-navy-600 transition-colors duration-150 "
         >
           <LogInIcon className="size-3.5 text-navy-200" />
         </button>
